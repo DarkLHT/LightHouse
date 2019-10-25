8c77fc249eea1116a253c0b0053d40cf47e65981
hmzamth <hamidhamza18@gmail.com>
2019-10-25 11:00:04 -0400

    Set And configure SMTP server

--- mail.php
+++ mail.php
@@ -1,59 +1,77 @@
-<?php    
-    // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        
-        // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-        $subject = strip_tags(trim($_POST["subject"]));
-        $number = strip_tags(trim($_POST["number"]));
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST["message"]);
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-        echo $name;
-        echo $subject;
-        echo $number;
-        echo $email;
-        echo $message;
+// Include PHPMailer library files
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
-        // Check that data was sent to the mailer.
-        if ( empty($name) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Set a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo "Oops! Message Not Send.";
-            exit;
-        }
+$mail = new PHPMailer;
 
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "sofian.beddi@gmail.com";
+// SMTP configuration
+$mail->isSMTP();
+$mail->Host     = 'mail.lighthouse.ma';
+$mail->SMTPAuth = true;
+$mail->Username = 'mail.smtp@lighthouse.ma';
+$mail->Password = 'B4psquq47*lyHoBg';
+$mail->SMTPSecure = 'tls';
+$mail->Port     = 587;
 
-        // Set the email subject.
-        $subject = "New Message from $name";
+ $name = strip_tags(trim($_POST["name"]));
+ $subject = strip_tags(trim($_POST["subject"]));
+ $number = strip_tags(trim($_POST["number"]));
+ $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+ $message = trim($_POST["message"]);
 
-        // Build the email content.
-        $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Subject: $subject\n\n";
-        $email_content .= "Number: $number\n\n";
-        $email_content .= "Message:\n$message\n";
 
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
+$mail->setFrom($email);
+//$mail->addReplyTo('contact@lighthouse.ma', 'CodexWorld');
 
-        // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thanks! Message has been sent successfully.";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong. Please try again.";
-        }
+// Add a recipient
+$mail->addAddress('contact@lighthouse.ma');
 
-    } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "Oops! Something went wrong.";
-    }
+// Add cc or bcc 
+
+// Email subject
+$mail->Subject = $subject;
+
+// Set email format to HTML
+$mail->isHTML(true);
+
+//$mail->SMTPDebug = 3;
+
+$mail->SMTPOptions = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
+
+// Email body content
+$mailContent = $message;
+$mail->Body = $mailContent;
+
+// Send email
+if(!$mail->send()){
+    echo 'Message could not be sent.';
+    //echo 'Mailer Error: ' . $mail->ErrorInfo;
+header( "refresh:1; url=index.html" );
+
+}else{
+    echo 'Message has been sent';
+header( "refresh:1; url=index.html" );
+}
+
+
+
+
+
+
+
+
+
+
+
 ?>
